cea86d3c4ff68f9905485e38c459e4016f7de849
Arrebarre34 <b24arvbe@student.his.se>
2025-11-10 15:18:55 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,3 +1,31 @@
 <?php
-      $pdo = new PDO('mysql:dbname=Kravion - Mölndals vårdcentral;host=localhost', "sqllab", 'Armadillo#2025');
-        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+class Database {
+    private $host;
+    private $port;
+    private $db_name;
+    private $username;
+    private $password;
+    private $conn;
+
+    public function __construct() {
+        $this->host = getenv('PGHOST') ?: 'localhost';
+        $this->port = getenv('PGPORT') ?: '3306';
+        $this->db_name = getenv('PGDATABASE') ?: 'healthcare';
+        $this->username = getenv('PGUSER') ?: 'sqllab';
+        $this->password = getenv('PGPASSWORD') ?: 'Pangolin!24';
+    }
+
+    public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $dsn = "pgsql:host=193.93.250.83" . $this->host . ";port=3306" . $this->port . ";dbname=wwwit-utv" . $this->db_name;
+            $this->conn = new PDO($dsn, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            echo "Connection Error: " . $e->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
