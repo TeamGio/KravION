73b6fd3d655e6d6438bdb643dd014421c80eefc6
TeamGio <Giordano.palomera@gmail.com>
2025-12-04 20:40:17 +0100

    Update dashboard.php

--- patient/dashboard.php
+++ patient/dashboard.php
@@ -1,4 +1,12 @@
 <?php
+
+
+// --- SKVALLER-KOD (Ta bort sen) ---
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+// ----------------------------------
+
 session_start();
 require_once '../config/database.php'; 
 require_once '../config/exempelfil_erp.php';
