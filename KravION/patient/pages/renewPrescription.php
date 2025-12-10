06efb2f9db02015950c2dca21ed7b4633cbbd113
Arrebarre34 <b24arvbe@student.his.se>
2025-12-10 15:30:27 +0100

    Parametrar
    Ändrade parameter

--- KravION/patient/pages/renewPrescription.php
+++ KravION/patient/pages/renewPrescription.php
@@ -10,6 +10,8 @@ error_reporting(E_ALL);
 session_start();
 require_once '../../config/exempelfil_erp.php';
 
+$renewPrescriptions = $erp_client->renewPrescriptions($prescription_id);
+
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(405);
